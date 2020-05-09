6550fd988d37bbf56c241eb240edd1cce8fe7a2d
osala-eng <jashonosala@gmail.com>
2020-05-09 18:32:05 +0300

    Express run

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app=express();
 
+app.use(express.static(__dirname +'/public'));
+
 app.get('/', function(request, response)
 {
-    response.send("Server is up and running")
-})
-app.listene(3000, function(){
+    response.send('Server is up and running')
+});
+
+app.listen(3000, function(){
     console.log("Server is running on port 3000")
 });
